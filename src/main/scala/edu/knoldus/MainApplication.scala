56c0074a45b656d702ba1b09a45b483071b19fa1
Kritikakhandelwal <kritika.khandelwal@knoldus.in>
2018-02-26 10:56:28 +0530

    changesMade

--- src/main/scala/edu/knoldus/MainApplication.scala
+++ src/main/scala/edu/knoldus/MainApplication.scala
@@ -3,6 +3,7 @@ package edu.knoldus
 import java.util.{Calendar, Date}
 
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -44,12 +45,31 @@ object Application extends App {
   val salesFile1 = sc.textFile("/home/knoldus/Desktop/File2.txt")
   val customerfile = customerFile1.map(line => line.split("#"))
   val salesFile = salesFile1.map(line => line.split('#'))
-  val customerData = customerfile.map(x =>Customer(x(0).toInt, x(1), x(2), x(3), x(4),x(5).toInt)).toDF()
-  val salesData = salesFile.map(x => Sales(Time.getYear(x(0).toInt), Time.getMonth(x(0).toInt), Time.getDay(x(0).toInt), x(1).toInt, x(2).toDouble))
+  val customerData = customerfile.map(x =>(x(0).toInt,Customer(x(0).toInt, x(1), x(2), x(3), x(4),x(5).toInt)))
+  val salesData = salesFile.map(x =>(x(1).toInt,Sales(Time.getYear(x(0).toInt), Time.getMonth(x(0).toInt), Time.getDay(x(0).toInt), x(1).toInt, x(2).toDouble)))
 
+  val innerJoin = customerData join salesData
 
-  val innerJoin = salesData.join(customerData, salesData("customer_id") === customerData("customer_id"), "inner")
-  innerJoin.select("state", "year", "price").show
+  val yearlyRecord = innerJoin.map(x => ((x._2._1.state, x._2._2.year), x._2._2.price))
+    .reduceByKey(_ + _).map(y => s"${y._1._1}#${y._1._2}###${y._2}")
+
+  val monthlyRecord = innerJoin.map(x => ((x._2._1.state, x._2._2.year, x._2._2.month), x._2._2.price))
+    .reduceByKey(_ + _).map(y => s"${y._1._1}#${y._1._2}#${y._1._3}##${y._2}")
+
+  val dailyRecord = innerJoin.map(x => ((x._2._1.state, x._2._2.year, x._2._2.month, x._2._2.day), x._2._2.price))
+    .reduceByKey(_ + _).map(y => s"${y._1._1}#${y._1._2}#${y._1._3}#${y._1._4}#${y._2}")
+
+  val record = yearlyRecord union  monthlyRecord union  dailyRecord
+
+  record.repartition(1);
+
+  record.saveAsTextFile("/home/knoldus/Desktop/result.txt");
+
+  println("\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n")
+  println(s"${yearlyRecord.collect.toList}\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+  println(s"${monthlyRecord.collect.toList}\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+  println(s"${dailyRecord.collect.toList}\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
   sc.stop()
 
 
